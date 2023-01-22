c4fb15b4784c7e6133aa92eccbaf9189bea3d4e8
thingsthatdothings <91434622+thingsthatdothings@users.noreply.github.com>
2023-01-22 16:48:32 -0700

    Delete actions.js

--- .github/workflows/actions.js
+++
@@ -1,8 +0,0 @@
-// const { setFailed, getInput, debug } = require( '@actions/core' );
-// const { context, getOctokit } = require( '@actions/github' );
-// 
-// ( async function main() {
-// 	debug( 'Our action is running' );
-// } )();
-
-console.log("hello world");
