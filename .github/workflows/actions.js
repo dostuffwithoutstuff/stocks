319603daf18cb97fb3f2cda6f97cd68ea92eea1b
thingsthatdothings <91434622+thingsthatdothings@users.noreply.github.com>
2023-01-22 16:35:22 -0700

    Update actions.js

--- .github/workflows/actions.js
+++ .github/workflows/actions.js
@@ -1,6 +1,8 @@
-const { setFailed, getInput, debug } = require( '@actions/core' );
-const { context, getOctokit } = require( '@actions/github' );
+// const { setFailed, getInput, debug } = require( '@actions/core' );
+// const { context, getOctokit } = require( '@actions/github' );
+// 
+// ( async function main() {
+// 	debug( 'Our action is running' );
+// } )();
 
-( async function main() {
-	debug( 'Our action is running' );
-} )();
+console.log("hello world");
